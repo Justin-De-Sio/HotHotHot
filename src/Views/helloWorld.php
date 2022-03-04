e64bb793124ae52b31ff0a18d6b4fac3ee2175b7
Baraka Ahmed <baraka.ahmed@etu.univ-amu.fr>
2022-03-04 10:51:34 +0100

    Première ébauche
    Sticky  Navbar

--- src/Views/helloWorld.php
+++ src/Views/helloWorld.php
@@ -1,4 +1,29 @@
 <?php View::setTitle('Page de bienvenue'); ?>
 <?php View::addStyleSheets('/css/style1.css') ;?>
 <?php View::addScript('hello','/js/hello.js',true) ;?>
-<h2 class="title">- <?php echo $A_view['messageContent'] ?> -</h2>
+
+<div class="content">
+        <h3>Sticky Navbar test</h3>
+        <p>Some text to enable scrolling.. Lorem ipsum dolor sit amet, illum definitiones no quo, maluisset concludaturque
+            et eum, altera fabulas ut quo. Atqui causae gloriatur ius te, id agam omnis evertitur eum. Affert laboramus
+            repudiandae nec et. Inciderint efficiantur his ad. Eum no molestiae voluptatibus.</p>
+        <p>Some text to enable scrolling.. Lorem ipsum dolor sit amet, illum definitiones no quo, maluisset concludaturque
+            et eum, altera fabulas ut quo. Atqui causae gloriatur ius te, id agam omnis evertitur eum. Affert laboramus
+            repudiandae nec et. Inciderint efficiantur his ad. Eum no molestiae voluptatibus.</p>
+        <p>Some text to enable scrolling.. Lorem ipsum dolor sit amet, illum definitiones no quo, maluisset concludaturque
+            et eum, altera fabulas ut quo. Atqui causae gloriatur ius te, id agam omnis evertitur eum. Affert laboramus
+            repudiandae nec et. Inciderint efficiantur his ad. Eum no molestiae voluptatibus.</p>
+        <p>Some text to enable scrolling.. Lorem ipsum dolor sit amet, illum definitiones no quo, maluisset concludaturque
+            et eum, altera fabulas ut quo. Atqui causae gloriatur ius te, id agam omnis evertitur eum. Affert laboramus
+            repudiandae nec et. Inciderint efficiantur his ad. Eum no molestiae voluptatibus.</p>
+        <p>Some text to enable scrolling.. Lorem ipsum dolor sit amet, illum definitiones no quo, maluisset concludaturque
+            et eum, altera fabulas ut quo. Atqui causae gloriatur ius te, id agam omnis evertitur eum. Affert laboramus
+            repudiandae nec et. Inciderint efficiantur his ad. Eum no molestiae voluptatibus.</p>
+        <p>Some text to enable scrolling.. Lorem ipsum dolor sit amet, illum definitiones no quo, maluisset concludaturque
+            et eum, altera fabulas ut quo. Atqui causae gloriatur ius te, id agam omnis evertitur eum. Affert laboramus
+            repudiandae nec et. Inciderint efficiantur his ad. Eum no molestiae voluptatibus.</p>
+        <p>Some text to enable scrolling.. Lorem ipsum dolor sit amet, illum definitiones no quo, maluisset concludaturque
+            et eum, altera fabulas ut quo. Atqui causae gloriatur ius te, id agam omnis evertitur eum. Affert laboramus
+            repudiandae nec et. Inciderint efficiantur his ad. Eum no molestiae voluptatibus.</p>
+
+    </div>
--- src/Views/standard/head.php
+++ src/Views/standard/head.php
@@ -1,2 +1,28 @@
-<?php
- echo '<header> <h1>Titre</h1></header>';
+<header>     
+    <div class="header">
+        <h2>Test Navbar</h2>
+        <p>Est-ce que la sticky navbar marche ?</p>
+    </div>
+
+    <nav id="navbar">
+        <a class="active" href="#home">Accueil</a>
+        <a href="#account">Mon profil</a>
+        <a href="#document">Documentation</a>
+        <a href="#disconnect">Déconnexion</a>
+    </nav>
+</header>
+
+<script> 
+    window.onscroll = function() {myFunction()};
+
+    var navbar = document.getElementById("navbar");
+    var sticky = navbar.offsetTop;
+
+    function myFunction() {
+        if (window.pageYOffset >= sticky) {
+            navbar.classList.add("sticky")
+        } else {
+            navbar.classList.remove("sticky");
+        }
+    }
+</script>
