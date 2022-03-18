7e7459a2f3da55e63c3efb0af7b271bf1fcb3567
Justin-De-Sio <desio.j@live.fr>
2022-03-18 19:08:35 +0100

    si la connection au websocket n'est pas encore ouverte alors l'api se lance toute les 10sec jusqu'a que l'état change https://developer.mozilla.org/fr/docs/Web/API/WebSocket/readyState

--- assets/js/capteur.js
+++ assets/js/capteur.js
@@ -1,43 +1,41 @@
 /*Websocket*/
-
 var socket = new WebSocket('wss://ws.hothothot.dog:9502');
-if (socket.onopen) {
-    socket.onopen = () => {
-        // Display user friendly messages for the successful establishment of connection
-        let label = document.getElementById("status");
-        setTimeout(() => {
-            if (socket.readyState === 1) {
-                socket.send("coucou !");
-                label.innerHTML = "Connexion établie";
-            } else {
-                console.log(`état socket.readyState${socket.readyState}`);
 
-            }
-        }, 5000)
-    }
-    socket.onmessage = (event) => {
-        var datas = document.getElementById("datas");
-        if (event.data)
-            datas.innerHTML = event.data
-        else
-            datas.innerHTML = "pas de donner"
+socket.onopen = () => {
+    // Display user friendly messages for the successful establishment of connection
+    let label = document.getElementById("status");
+    setTimeout(() => {
+        if (socket.readyState === 1) {
+            socket.send("coucou !");
+            label.innerHTML = "Connexion établie";
+        } else {
+            console.log(`état socket.readyState${socket.readyState}`);
+        }
+    }, 5000)
+}
+socket.onmessage = (event) => {
+    var datas = document.getElementById("datas");
+    if (event.data) {
+        datas.innerHTML = event.data
         var msg = JSON.parse(event.data)
         subject.Notify(msg)
-
-    }
-} else {
-    //se connecter a une API
-
-    console.log("connexion au websocket impossible. connexion à l'API...")
-    let label = document.getElementById("status");
-    label.innerHTML = "Connexion API";
-
-    const apiCapteurs = 'https://hothothot.dog/api/capteurs'
-    fetch(apiCapteurs)
-        .then((response) => response.json()
-            .then((data) => {
-                subject.Notify(data)
-                console.log(data)
-            }))
-
+    } else
+        datas.innerHTML = "pas de donner"
 }
+setInterval(() => {
+    if (socket.readyState !== 1) {
+        console.log("connexion au websocket impossible. connexion à l'API...")
+        let label = document.getElementById("status");
+        label.innerHTML = "Connexion API";
+
+        const apiCapteurs = 'https://hothothot.dog/api/capteurs'
+        fetch(apiCapteurs)
+            .then((response) => response.json()
+                .then((data) => {
+                    subject.Notify(data)
+                    console.log(data)
+                }))
+    }
+}, 10000)
+
+
