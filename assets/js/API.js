e4fa5a71ddb651426772bdc28b09c884bcb3e1e9
Justin-De-Sio <desio.j@live.fr>
2022-03-20 12:34:57 +0100

    rename + historique en local

---
+++ assets/js/API.js
@@ -0,0 +1,38 @@
+// /*Websocket*/
+// var socket = new WebSocket('wss://ws.hothothot.dog:9502');
+//
+// socket.onopen = () => {
+//     // Display user friendly messages for the successful establishment of connection
+//     setTimeout(() => {
+//         if (socket.readyState === 1) {
+//             socket.send("coucou !");
+//             console.log("Connexion établie au serveur de websocket");
+//         } else {
+//             console.log(`état socket.readyState${socket.readyState}`);
+//         }
+//     }, 5000)
+// }
+// socket.onmessage = (event) => {
+//     if (event.data) {
+//         let data = JSON.parse(event.data);
+//         subject.Notify(data)
+//     } else
+//         console.log("pas de donné")
+// }
+setInterval(() => {
+    if (/*socket.readyState !==*/ 1) {
+        console.log("connexion au websocket impossible. connexion à l'API...")
+        let label = document.getElementById("status");
+        label.innerHTML = "Connexion API";
+
+        const apiCapteurs = 'https://hothothot.dog/api/capteurs'
+        fetch(apiCapteurs)
+            .then((response) => response.json()
+                .then((data) => {
+                    subject.Notify(data)
+
+                }))
+    }
+}, 10000)
+
+
--- assets/js/mvc.js
+++ assets/js/mvc.js
@@ -1,39 +1,30 @@
-class SensorController {
-    constructor(sensorModel, sensorView) {
-        this.sensorModel = sensorModel;
-        this.sensorView = sensorView;
-    }
-
-}
-
 class SensorModel {
-
 
     constructor() {
         this._history = JSON.parse(localStorage.getItem('history')) || []
-
+        this.update = this.update.bind(this)
     }
 
 
     update(data) {
         // c'est ici que l'on met tout nos methods à activé à chaque mise à jour
-        this.interiorData = data.capteurs[0];
-        this.interiorValue = this.interiorData['Valeur'];
-        this.exteriorData = data.capteurs[1];
-        this.exteriorValue = this.exteriorData['Valeur'];
+        let capteurs = data.capteurs
 
-        this.time = this.interiorData.Timestamp;
-
-
+        // let interiorData = capteurs[0];
+        // this.interiorValue = interiorData['Valeur'];
+        // let exteriorData = capteurs[1];
+        // this.exteriorValue = exteriorData['Valeur'];
+        // this.time = interiorData.Timestamp;
+        this.addDate(capteurs)
         // this.dataCapteurExtrreiur = data.capteurs[1];
-        // app.sensorView.displayDataByConsole(this.dataCapteurExtrreiur.Valeur )// Console.log
-        // this._history.push(this.data)
-        // this._commit(this._history)
+        // app.sensorView.displayDataByConsole(this.exteriorValue)// Console.log
 
     }
 
-
-
+    addDate(data) {
+        this._history.push(data)
+        this._commit(this._history)
+    }
 
     get history() {
         this._history = JSON.parse(localStorage.getItem('history')) || []
@@ -41,6 +32,7 @@ class SensorModel {
     }
 
     _commit(history) {
+        this.onHistoryChanged(this.history)
         localStorage.setItem('history', JSON.stringify(history))
     }
 
@@ -56,8 +48,24 @@ class SensorView {
     displayDataByConsole(data) {
         console.log("nouvelle temperature : " + data)
     }
+
+    displayHistory(history) {
+        this.to
+    }
+}
+
+class SensorController {
+    constructor(sensorModel, sensorView) {
+        this.sensorModel = sensorModel;
+        this.sensorView = sensorView;
+
+        // Display initial data
+        this.onHistoryChanged(this.sensorModel._history)
+    }
+
+    onHistoryChanged = (history) => this.sensorView.displayHistory(history)
 }
 
 const app = new SensorController(new SensorModel(), new SensorView())
-var subject = new SensorSubject();
+var subject = new Publisher();
 subject.subscribe(app.sensorModel.update)
