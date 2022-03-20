4d8f41e6bd452039dcfeb2b99ca1533ff0c7db9c
Justin-De-Sio <desio.j@live.fr>
2022-03-20 14:31:05 +0100

    update

--- assets/js/API.js
+++ assets/js/API.js
@@ -1,25 +1,28 @@
-// /*Websocket*/
-// var socket = new WebSocket('wss://ws.hothothot.dog:9502');
-//
-// socket.onopen = () => {
-//     // Display user friendly messages for the successful establishment of connection
-//     setTimeout(() => {
-//         if (socket.readyState === 1) {
-//             socket.send("coucou !");
-//             console.log("Connexion établie au serveur de websocket");
-//         } else {
-//             console.log(`état socket.readyState${socket.readyState}`);
-//         }
-//     }, 5000)
-// }
-// socket.onmessage = (event) => {
-//     if (event.data) {
-//         let data = JSON.parse(event.data);
-//         subject.Notify(data)
-//     } else
-//         console.log("pas de donné")
-// }
-setInterval(() => {
+/*Websocket*/
+var socket = new WebSocket('wss://ws.hothothot.dog:9502');
+
+socket.onopen = () => {
+    // Display user friendly messages for the successful establishment of connection
+    setTimeout(() => {
+        if (socket.readyState === 1) {
+            socket.send("coucou !");
+            console.log("Connexion établie au serveur de websocket");
+        } else {
+            console.log(`état socket.readyState${socket.readyState}`);
+        }
+    }, 5000)
+}
+socket.onmessage = (event) => {
+    if (event.data) {
+        let data = JSON.parse(event.data);
+        subject.Notify(data)
+    } else
+        console.log("pas de donné")
+}
+
+setInterval(api, 10000)
+
+function api() {
     if (/*socket.readyState !==*/ 1) {
         console.log("connexion au websocket impossible. connexion à l'API...")
         let label = document.getElementById("status");
@@ -33,6 +36,4 @@ setInterval(() => {
 
                 }))
     }
-}, 10000)
-
-
+}
