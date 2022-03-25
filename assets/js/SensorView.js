fb470ea7ca84ad5028ee5741b79e16a98443b6ba
Justin-De-Sio <desio.j@live.fr>
2022-03-25 20:14:23 +0100

    comment

--- assets/js/SensorView.js
+++ assets/js/SensorView.js
@@ -28,11 +28,15 @@ class SensorView {
         console.log(history)
     }
 
-    removeChild(parentElement){
+
+    // supprime les anciens affichages de temperature
+    removeChild(parentElement) {
         while (parentElement.firstChild) {
             parentElement.removeChild(parentElement.firstChild)
         }
     }
+
+    //affiche les donnés de la dernière capture de donné
     displayLastData(history) {
         this.removeChild(this.displayList)
 
@@ -41,30 +45,31 @@ class SensorView {
             for (let capteurIndex = 0; capteurIndex < lastCapteurs.length; capteurIndex++) {
 
                 this.listValuesCapteur = []
+
+                // récupère toutes les températures d'un capteur spécifique
                 for (const historyElement of history)
                     this.listValuesCapteur.push(historyElement[capteurIndex]['Valeur']);
-
                 this.listValuesCapteur = this.listValuesCapteur.map(Number) // str -> int
-                console.log(this.listValuesCapteur)
-                let capteur = lastCapteurs[capteurIndex]
 
-                console.log(`Nom:${capteur['Nom']} ; Valeur: ${capteur['Valeur']} degree ; min :${Math.min.apply(Math, this.listValuesCapteur)}; max :${Math.max.apply(Math, this.listValuesCapteur)}`)
+                const capteur = lastCapteurs[capteurIndex]
+
+                // console.log(`Nom:${capteur['Nom']} ; Valeur: ${capteur['Valeur']} degree ; min :${Math.min.apply(Math, this.listValuesCapteur)}; max :${Math.max.apply(Math, this.listValuesCapteur)}`)
 
 
                 this.displayElement = this.createElement("li", capteur['Nom'])
 
                 this.displayList.append(this.displayElement)
+
                 this.temperature_name = this.createElement("p", "temperature_name")
-
                 this.temperature_name.textContent = `Nom:${capteur['Nom']}`
+
                 this.temperature_value = this.createElement("p", "temperature_value")
-
                 this.temperature_value.textContent = `Valeur: ${capteur['Valeur']} °C`
+
                 this.temperature_maximum = this.createElement("p", "temperature_maximum")
-
                 this.temperature_maximum.textContent = `max :${Math.max.apply(Math, this.listValuesCapteur)}`
-                this.temperature_minimum = this.createElement("p", "temperature_minimum")
 
+                this.temperature_minimum = this.createElement("p", "temperature_minimum")
                 this.temperature_minimum.textContent = `min :${Math.min.apply(Math, this.listValuesCapteur)}`
 
                 this.displayElement.append(
